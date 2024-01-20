180f7aecc3a2de84ffab187997ce1db4efba324f
Kamila <89898648749@mail.ru>
2024-01-20 20:17:55 +0300

    Выводим массив

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿
+string[] array = new string[4]{"hello", "cat", "21", "work"};
+
+void PrintArray()
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+PrintArray();
