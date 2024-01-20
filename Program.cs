5c21fd28d2172764b98283f87751275db291aa9e
Kamila <89898648749@mail.ru>
2024-01-20 21:30:16 +0300

    Ищем строки с длиной меньше или равной 3 и записываем их в новый массив

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿
 string[] array = new string[4]{"hello", "cat", "21", "work"};
+string[] newArray = new string[4];
 
 void PrintArray()
 {
-    for(int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]}, ");
-    }
+   Console.Write($"Начальный массив: [{string.Join(", ", array)}]");
 }
 
+int index = array.Length - 1;
+
+void PrintNewArray(string[] array, int index, int index2)
+{
+    int count = 0;
+
+    if(index < 0)
+    {
+        return;
+    }
+    
+    for(int i = 0; i < array[index].Length; i++)
+    {
+        count++;
+    }
+
+    if(count <= 3)
+    {
+        newArray[index2] = array[index];
+        Console.Write(newArray[index2] + ", ");
+    }
+
+    PrintNewArray(array, index - 1, index2 + 1);
+}
+
+
 PrintArray();
+Console.WriteLine();
+Console.Write("Конечный массив: [");
+PrintNewArray(array, index, 0);
+Console.Write("]");
